5438bee4b6c345c75893fb406de22ffd34d82897
June <easdkr@daum.net>
2024-02-18 00:28:57 +0900

    [fix] orm 설정시 host 누락 수정

--- libs/database/datasource.ts
+++ libs/database/datasource.ts
@@ -2,6 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const entitiesPath = `${__dirname}/../../**/*.entity.{js,ts}`;
 const migrationPath = `${__dirname}/migrations/**/*.{js,ts}`;
+console.log(process.env.DATABASE_NAME);
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -9,6 +10,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   port: +process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
   entities: [entitiesPath],
   migrations: [migrationPath],
   logging: true,
